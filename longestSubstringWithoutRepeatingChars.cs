5a077a44bad55219496171f4759f640afa90d351
Redbeard29 <ben@benshermanmusic.com>
2022-02-19 13:33:42 -0500

    palindromeNumber second solution

--- longestSubstringWithoutRepeatingChars.cs
+++ longestSubstringWithoutRepeatingChars.cs
@@ -6,33 +6,7 @@ namespace CSharp
     {
 
     public int getLengthOfLongestSubstring(string s) {
-        
-        int start = 0;
-        int longestSub = 0;
-        
-        Dictionary<char, int> charIndexDict = new Dictionary<char, int>();
-        
-        for(int end = 0; end < s.Length; end++){
-            
-            if(!(charIndexDict.ContainsKey(s[end]))){
-                charIndexDict.Add(s[end], end);
-            }
-            
-            else{
-                if(end - start > longestSub){
-                    longestSub = end - start;
-                }
-                
-                start = end;
-            }
-            
-        }
-        
-        foreach(KeyValuePair<char, int> kvp in charIndexDict){
-            Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-        }
-        
-        return longestSub;
+        return 0;
             
     }
     }
--- palindromeNumberSecondSolution.cs
+++ palindromeNumberSecondSolution.cs
@@ -6,6 +6,24 @@ namespace CSharp
     {
         public bool isPalindrome(int x){
 
+            if(x < 0){
+                return false;
+            }
+            
+            string intAsString = x.ToString();
+
+            int left = 0;
+            int right = intAsString.Length - 1;
+
+            while(left <= right){
+                if(intAsString[left] != intAsString[right]){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
         }
         
     }
